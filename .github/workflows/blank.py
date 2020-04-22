7a9861694e8dfdfba303fe7c6b321e673b17dfba
maksim992538 <64153700+maksim992538@users.noreply.github.com>
2020-04-22 19:05:34 +0300

    Update blank.py

--- .github/workflows/blank.py
+++ .github/workflows/blank.py
@@ -1,19 +1,19 @@
 import os
 from win10toast import ToastNotifier
 toaster = ToastNotifier()
-print ("Команда запущена!")
-with open("IPs.txt","r+") as file:
-        for line in file:
-            response =  os.system("ping   " + line)
-        if response == 0:
-            print("Связь есть:")
-            print(line)
-
-        else:
-             with open("cam.txt","a+") as file:
-                 aunt1 = line
-                 toaster.show_toast("IP-адрес не доступен",aunt1,icon_path="custom.ico",duration=2)
-                 print(line + "IP-адрес не доступен")
-                 file.write("IP-адрес не доступен:")
-                 file.write(line)
+i = 1
+while i<10:
+    file = open("IPs.txt","r+")
+    with open("IPs.txt","r+") as file:
+            for line in file:
+                response =  os.system("ping   " + line)
+            if response == 0:
+                print(line + "Связь есть!")
+            else:
+                with open("cam.txt","a+") as file:
+                    aunt1 = line
+                    toaster.show_toast("IP-адрес не доступен",aunt1,icon_path="custom.ico",duration=2)
+                    print(line + "IP-адрес не доступен")
+                    file.write("IP-адрес не доступен:")
+                    file.write(line)
 
